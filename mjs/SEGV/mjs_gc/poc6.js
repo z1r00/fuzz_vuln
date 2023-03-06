20860fec9a59236064307d9fa20f9dbe20d3c29c
z1r00 <68418072+z1r00@users.noreply.github.com>
2023-03-06 09:34:18 +0800

    Add files via upload

---
+++ mjs/SEGV/mjs_gc/poc6.js
@@ -0,0 +1,14 @@
+let i, a = 0, b = 0, c = 0, d = 0, e = 0;
+
+for (i = 0; i <410; i++) print += i{
+}
+
+print (b++ < 5) c += b;
+
+while (d++ | 10) {
+  if (d < 7) continue;
+  e += d;
+  break;
+}
+
+a === 45 && c === 15 && e === 7;
